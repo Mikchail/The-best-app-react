29c819b132a6d5173dc30eceec116f83d81482ed
Mikchail <refs1@yandex.ru>
2020-11-22 14:39:54 +0400

    add build

---
+++ client/build/precache-manifest.cf56d9cef2432d04e1b370222ec7c9a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "3eba6e89bab685e8a151",
+    "url": "/static/css/main.6cde73fd.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "102e11d77b8e44b11fe8",
+    "url": "/static/js/2.4de70e07.chunk.js"
+  },
+  {
+    "revision": "3eba6e89bab685e8a151",
+    "url": "/static/js/main.89fcf264.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "102e11d77b8e44b11fe8",
+    "url": "/static/css/2.5fa93a21.chunk.css"
+  },
+  {
+    "revision": "f10c39b540047ce2eca8a6212fac3dba",
+    "url": "/index.html"
+  }
+];
